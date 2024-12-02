05c0109c1dfe64df73c3c656d654ce5ae31cf159
Lutz Mitländer <lutzmitlaender@gmx.net>
2024-12-02 12:51:23 +0100

    Day 2 Part 2 solution + actually I lied and I added one additional crate for heap analysis (dhat)

--- src/days/day2.rs
+++ src/days/day2.rs
@@ -28,7 +28,7 @@ fn part1(input: &Vec<String>) {
         }
     }
 
-    println!("Safe levels: {}", safe);
+    println!("Safe levels Part 1: {}", safe);
 }
 
 fn recurse_test_asc(input: &Vec<i32>, index: usize) -> bool {
@@ -56,5 +56,56 @@ fn recurse_test_desc(input: &Vec<i32>, index: usize) -> bool {
 }
 
 fn part2(input: &Vec<String>) {
+    let mut safe = 0;
     
+    for i in 0..input.len() {
+        let split_integers: Vec<i32> = input[i].split(" ").map(|x| x.trim().parse::<i32>().unwrap()).collect();
+
+        if part2_recurse_test_asc(&mut split_integers.clone(), 1, false) {
+            safe += 1;
+        } else if part2_recurse_test_desc(&mut split_integers.clone(), 1, false) {
+            safe += 1;
+        }
+    }
+
+    println!("Safe levels Part 2: {}", safe);
+}
+
+fn part2_recurse_test_asc(input: &mut Vec<i32>, index: usize, had_false: bool) -> bool {
+    if index == input.len() {
+        return true;
+    }
+
+    if index == 0 || (input[index] > input[index - 1] && (input[index] - input[index - 1]).abs() <= 3 && (input[index] - input[index - 1]).abs() > 0) {
+        return part2_recurse_test_asc(input, index + 1, had_false);
+    }
+
+    if !had_false {
+        let mut input_cloned = input.clone();
+        input.remove(index - 1);
+        input_cloned.remove(index);
+        return part2_recurse_test_asc(input, index - 1, true) || part2_recurse_test_asc(&mut input_cloned.clone(), index - 1, true);
+    }
+
+    return false;
+}
+
+fn part2_recurse_test_desc(input: &mut Vec<i32>, index: usize, had_false: bool) -> bool {
+    if index == input.len() {
+        return true;
+    }
+
+    if index == 0 || (input[index] < input[index - 1] && (input[index] - input[index - 1]).abs() <= 3 && (input[index] - input[index - 1]).abs() > 0) {
+        return part2_recurse_test_desc(input, index + 1, had_false);
+    }
+
+    if !had_false {
+        let mut input_cloned = input.clone();
+        input.remove(index - 1);
+        input_cloned.remove(index);
+        
+        return part2_recurse_test_desc(input, index - 1, true) || part2_recurse_test_desc(&mut input_cloned.clone(), index - 1, true);
+    }
+
+    return false;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,14 @@ use std::io;
 mod days;
 mod utils;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 fn main() {
+    #[cfg(feature = "dhat-heap")]
+    let _profiler = dhat::Profiler::new_heap();
+
     let mut input = String::new();
     println!("Enter the day of Advent of Code:");
     io::stdin().read_line(&mut input).expect("Failed to read line");
